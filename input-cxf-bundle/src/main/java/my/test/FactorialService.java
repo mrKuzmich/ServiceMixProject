8b290454d334fe8116c9d2c6d3b9222db425c9cd
Dmitry Kuzmin <kuzmich.murom@gmail.com>
2019-04-30 01:21:02 +0300

    Restore service

--- input-cxf-bundle/src/main/java/my/test/FactorialService.java
+++ input-cxf-bundle/src/main/java/my/test/FactorialService.java
@@ -1,15 +1,38 @@
 package my.test;
 
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.Session;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import java.math.BigInteger;
 
 @Path("/maths/")
 public class FactorialService {
+    private QueueConnectionFactory connectionFactory;
+
     @GET
-    @Path("/factorial/{n}")
-    public String factorial(@PathParam("n") String strN) {
+    @Path("/getFactorial/{n}")
+    public String getFactorial(@PathParam("n") String strN) {
         System.out.println("Get factorial " + strN);
-        return "fuck(1)";
+        final Connection connection = connectionFactory.createQueueConnection();
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        session.createProducer()
+        return String.format("%s! = %d", strN, getFactorial(Integer.valueOf(strN)));
+    }
+
+    private BigInteger getFactorial(int n) {
+        return n <= 1 ? BigInteger.ONE : BigInteger.valueOf(n).multiply(getFactorial(n -1));
+    }
+
+    public ConnectionFactory getConnectionFactory() {
+        return connectionFactory;
+    }
+
+    public void setConnectionFactory(ConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
     }
 }
