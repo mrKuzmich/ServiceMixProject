bbf101c240b064c915b16c3ad2ce93155ee0c0e2
Dmitry Kuzmin <kuzmich.murom@gmail.com>
2019-05-12 00:04:43 +0300

    First worked version

--- input-cxf-bundle/src/main/java/my/test/input_cxf_bundle/FactorialService.java
+++ input-cxf-bundle/src/main/java/my/test/input_cxf_bundle/FactorialService.java
@@ -1,6 +1,5 @@
 package my.test.input_cxf_bundle;
 
-import my.test.calc_bundle.calculator.FactorialCalculatorImpl;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -10,69 +9,77 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
-import java.math.BigInteger;
 
 @Path("/maths/")
 public class FactorialService {
-    private Log LOGGER = LogFactory.getLog(FactorialService.class);
+  private Log LOGGER = LogFactory.getLog(FactorialService.class);
 
-    private ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
-    private Connection jmsConnection;
+  private ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
+  private Connection jmsConnection;
 
-    @GET
-    @Path("/getFactorial/{n}")
-    public Response getFactorial(@PathParam("n") String strN) { // todo javax.validation
-        try {
-            LOGGER.debug(String.format("Request: getFactorial(%s)", strN));
-            sendRequestToQueue(strN);
-            final BigInteger result = getFactorial(Integer.valueOf(strN));
-            final String response = String.format("%s! = %d", strN, result);
-            LOGGER.debug("Response: " + response);
-            return Response.ok(response).build();
-        } catch (Exception e) {
-            final String errorMsg = String.format("getFactorial error: %s: %s",
-                    e.getClass().getCanonicalName(), e.getMessage());
-            LOGGER.debug("Error: " + errorMsg);
-            return Response
-                    .status(Response.Status.BAD_REQUEST)
-                    .entity(errorMsg)
-                    .build();
-        }
+  @GET
+  @Path("/getFactorial/{n}")
+  public Response getFactorial(@PathParam("n") String strN) { // todo javax.validation
+    Connection connection = null;
+    try {
+      LOGGER.debug(String.format("Request: getFactorial(%s)", strN));
+      connection = getConnectionFactory().createConnection("smx", "smx");
+      try {
+        connection.start();
+        String msgLabel = sendRequestToQueue(connection, strN);
+        final String result = getResponseFromQueue(connection, msgLabel);
+        connection.stop();
+        final String response = String.format("%s! = %s", strN, result);
+        LOGGER.debug("Response: " + response);
+        return Response.ok(response).build();
+      } finally {
+        if (connection != null) connection.close();
+      }
+    } catch (Exception e) {
+      final String errorMsg = String.format("getFactorial error: %s: %s",
+              e.getClass().getCanonicalName(), e.getMessage());
+      LOGGER.debug("Error: " + errorMsg);
+      return Response
+              .status(Response.Status.BAD_REQUEST)
+              .entity(errorMsg)
+              .build();
     }
+  }
 
-    private void sendRequestToQueue(String request) throws JMSException {
-        final Connection connection = getConnectionFactory().createConnection("smx", "smx");
-        final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        final Queue queue = session.createQueue("inputQueue");
-        final MessageProducer producer = session.createProducer(queue);
-        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-        final Message msg = session.createTextMessage(request);
-        producer.send(msg);
+  private String sendRequestToQueue(Connection connection, String request) throws JMSException {
+    Session session = null; // todo singleton?
+    try {
+      session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      final Queue queue = session.createQueue("inputQueue");
+      final MessageProducer producer = session.createProducer(queue);
+      producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+      final Message msg = session.createTextMessage(request);
+      // need to somehow labeled a message to filter the response
+      msg.setJMSCorrelationID("ID:" + request);
+      producer.send(msg);
+      return msg.getJMSCorrelationID();
+    } finally {
+      if (session != null)
         session.close();
     }
+  }
 
-    private String getResponseFromQueue() {
-        return null;
+  private String getResponseFromQueue(Connection connection, String msgLabel) throws JMSException, TimeoutException {
+    Session session = null;
+    try {
+      session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      final Queue queue = session.createQueue("outputQueue");
+      final MessageConsumer consumer = session.createConsumer(queue, "JMSCorrelationID='" + msgLabel + "'");
+      final Message response = consumer.receive(1000); // todo define variable
+      if (response == null) throw new TimeoutException("Calculator not response.");
+      return ((TextMessage) response).getText();
+    } finally {
+      if (session != null)
+        session.close();
     }
+  }
 
-    private BigInteger getFactorial(int n) {
-        //return Activator.getCalculator().apply(n);
-        return new FactorialCalculatorImpl().apply(n);
-    }
-
-    public ConnectionFactory getConnectionFactory() {
-        return connectionFactory;
-    }
-
-    public void setConnectionFactory(ConnectionFactory connectionFactory) {
-        this.connectionFactory = connectionFactory;
-    }
-
-    public Connection getJmsConnection() {
-        return jmsConnection;
-    }
-
-    public void setJmsConnection(Connection jmsConnection) {
-        this.jmsConnection = jmsConnection;
-    }
+  public ConnectionFactory getConnectionFactory() {
+    return connectionFactory;
+  }
 }
---
+++ input-cxf-bundle/src/main/java/my/test/input_cxf_bundle/TimeoutException.java
@@ -0,0 +1,7 @@
+package my.test.input_cxf_bundle;
+
+public class TimeoutException extends Exception {
+  public TimeoutException(String s) {
+    super(s);
+  }
+}
