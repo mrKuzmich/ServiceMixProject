9872eb3e74a17e0420801f1cd41b89918c7baf90
Dmitry Kuzmin <kuzmich.murom@gmail.com>
2019-05-13 01:22:10 +0300

    Added configuration of calculation algorithm

--- input-queue-bundle/src/main/java/my/test/input_queue_bundle/MathsOperations.java
+++ input-queue-bundle/src/main/java/my/test/input_queue_bundle/MathsOperations.java
@@ -1,14 +1,21 @@
 package my.test.input_queue_bundle;
 
-import my.test.calc_bundle.calculator.FactorialCalculatorImpl;
+import my.test.calc_bundle.calculator.FactorialCalculator;
 
 import java.util.Objects;
 
 public class MathsOperations {
+
+  private final FactorialCalculator calculator;
+
+  public MathsOperations(FactorialCalculator calculator) {
+    this.calculator = calculator;
+  }
+
   public Object Factorial(Object request) {
     try {
       Integer n = Integer.valueOf(Objects.requireNonNull(request, "Empty request").toString());
-      return new FactorialCalculatorImpl().apply(n).toString();
+      return calculator.apply(n).toString();
     } catch (Exception e) {
       return "Calculate Factorial exception: " + e.getClass().getCanonicalName() + ": " + e.getMessage();
     }
