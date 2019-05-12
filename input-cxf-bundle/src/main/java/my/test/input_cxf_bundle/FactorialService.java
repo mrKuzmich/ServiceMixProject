d22e42afcecbd4618f6dfbe10b004132550608a6
Dmitry Kuzmin <kuzmich.murom@gmail.com>
2019-05-12 17:08:03 +0300

    Redesigned on Spring JmsTemplate

--- input-cxf-bundle/src/main/java/my/test/input_cxf_bundle/FactorialService.java
+++ input-cxf-bundle/src/main/java/my/test/input_cxf_bundle/FactorialService.java
@@ -1,8 +1,8 @@
 package my.test.input_cxf_bundle;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.jms.core.JmsTemplate;
 
 import javax.jms.*;
 import javax.ws.rs.GET;
@@ -14,8 +14,8 @@ import javax.ws.rs.core.Response;
 public class FactorialService {
   private Log LOGGER = LogFactory.getLog(FactorialService.class);
 
-  private ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
-  private Connection jmsConnection;
+  private JmsTemplate sendJmsTemplate;
+  private JmsTemplate receiveJmsTemplate;
 
   @GET
   @Path("/getFactorial/{n}")
@@ -23,18 +23,11 @@ public class FactorialService {
     Connection connection = null;
     try {
       LOGGER.debug(String.format("Request: getFactorial(%s)", strN));
-      connection = getConnectionFactory().createConnection("smx", "smx");
-      try {
-        connection.start();
-        String msgLabel = sendRequestToQueue(connection, strN);
-        final String result = getResponseFromQueue(connection, msgLabel);
-        connection.stop();
+        String msgLabel = sendRequestToQueue(strN);
+        final String result = getResponseFromQueue(msgLabel);
         final String response = String.format("%s! = %s", strN, result);
         LOGGER.debug("Response: " + response);
         return Response.ok(response).build();
-      } finally {
-        if (connection != null) connection.close();
-      }
     } catch (Exception e) {
       final String errorMsg = String.format("getFactorial error: %s: %s",
               e.getClass().getCanonicalName(), e.getMessage());
@@ -46,40 +39,39 @@ public class FactorialService {
     }
   }
 
-  private String sendRequestToQueue(Connection connection, String request) throws JMSException {
-    Session session = null; // todo singleton?
-    try {
-      session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-      final Queue queue = session.createQueue("inputQueue");
-      final MessageProducer producer = session.createProducer(queue);
-      producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-      final Message msg = session.createTextMessage(request);
-      // need to somehow labeled a message to filter the response
-      msg.setJMSCorrelationID("ID:" + request);
-      producer.send(msg);
-      return msg.getJMSCorrelationID();
-    } finally {
-      if (session != null)
-        session.close();
-    }
+  private String sendRequestToQueue(String request) throws JMSException {
+    final String msgLabel = makeMsgLabel(request);
+    getSendJmsTemplate().convertAndSend(request, message -> {
+      message.setJMSCorrelationID(msgLabel);
+      return message;
+    });
+    return msgLabel;
   }
 
-  private String getResponseFromQueue(Connection connection, String msgLabel) throws JMSException, TimeoutException {
-    Session session = null;
-    try {
-      session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-      final Queue queue = session.createQueue("outputQueue");
-      final MessageConsumer consumer = session.createConsumer(queue, "JMSCorrelationID='" + msgLabel + "'");
-      final Message response = consumer.receive(1000); // todo define variable
-      if (response == null) throw new TimeoutException("Calculator not response.");
-      return ((TextMessage) response).getText();
-    } finally {
-      if (session != null)
-        session.close();
-    }
+  private String makeMsgLabel(String value) {
+    return "ID:" + value;
   }
 
-  public ConnectionFactory getConnectionFactory() {
-    return connectionFactory;
+  private String getResponseFromQueue(String msgLabel) throws JMSException, TimeoutException {
+    final Message response = getReceiveJmsTemplate()
+            .receiveSelected("JMSCorrelationID='" + msgLabel + "'");
+    if (response == null) throw new TimeoutException("Calculator not response.");
+    return ((TextMessage) response).getText();
+  }
+
+  public JmsTemplate getSendJmsTemplate() {
+    return sendJmsTemplate;
+  }
+
+  public void setSendJmsTemplate(JmsTemplate sendJmsTemplate) {
+    this.sendJmsTemplate = sendJmsTemplate;
+  }
+
+  public JmsTemplate getReceiveJmsTemplate() {
+    return receiveJmsTemplate;
+  }
+
+  public void setReceiveJmsTemplate(JmsTemplate receiveJmsTemplate) {
+    this.receiveJmsTemplate = receiveJmsTemplate;
   }
 }
