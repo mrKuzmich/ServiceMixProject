743a08d34610c238a501d1dbba282423ab074f1f
Dmitry Kuzmin <dmitry.kuzmin@red-soft.ru>
2019-05-13 12:48:56 +0300

    Fix caching algorithm

--- calc-bundle/src/main/java/my/test/calc_bundle/calculator/CachedFactorialCalculator.java
+++ calc-bundle/src/main/java/my/test/calc_bundle/calculator/CachedFactorialCalculator.java
@@ -4,13 +4,13 @@ import java.math.BigInteger;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class CachedFactorialCalculator extends SimpleFactorialCalculator {
+public class CachedFactorialCalculator implements FactorialCalculator {
 
   // TODO: this is a bad cache, need to use replacement policies
   private static Map<Integer, BigInteger> cache = new ConcurrentHashMap<>();
 
   @Override
   public BigInteger apply(Integer n) {
-    return cache.computeIfAbsent(n, super::apply);
+      return cache.computeIfAbsent(n, nn -> new SimpleFactorialCalculator().apply(nn));
   }
 }
